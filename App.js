0dd4942ed6b72b1123b1970f3f138318c8f80c04
Joseph <acheampongjoseph470@gmail.com>
2024-05-22 18:47:53 +0000

    update

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Screen1 from './components/Screen1';
+import Home from './components/Home'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Screen1 />
+      <Home />
       <StatusBar style="light" />
     </View>
   );
