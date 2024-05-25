238e2250805544eb619f9e9d428bec277cba7edd
Joseph <acheampongjoseph470@gmail.com>
2024-05-25 18:44:08 +0000

    new component

---
+++ components/Attraction.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Attraction() {
+  return (
+    <View>
+      <Text>Attraction</Text>
+    </View>
+  )
+}
