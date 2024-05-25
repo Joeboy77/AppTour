fe15861895889e7624673c74c00bc149a0ab57ce
Joseph <acheampongjoseph470@gmail.com>
2024-05-25 18:47:31 +0000

    update

--- components/Attraction.js
+++ components/Attraction.js
@@ -9,5 +9,10 @@ export default function Attraction() {
   )
 }
 const styles = StyleSheet.create({
-    
+    view: {
+        flex: 1,
+        backgroundColor: 'plum',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 })
