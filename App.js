442ea2f32bd76af491e0b8c7a0a87a9e22ddbc23
Joseph <acheampongjoseph470@gmail.com>
2024-05-27 15:25:59 +0000

    update

--- App.js
+++ App.js
@@ -4,11 +4,18 @@ import Screen1 from './components/Screen1';
 import Home from './components/Home';
 import LocationMap from './components/LocationMap';
 import Attraction from './components/Attraction';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home />
+      <Drawer.Navigator>
+        <Drawer.Screen name='Home' component={Home}/>
+        <Drawer.Screen name='Attraction' component={Attraction}/>
+      </Drawer.Navigator>
       <StatusBar style="dark" />
     </View>
   );
