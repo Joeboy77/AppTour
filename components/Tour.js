0cb9c0efea6bee84f4acda332e9c505fbffc7baa
Joseph <acheampongjoseph470@gmail.com>
2024-05-27 11:28:02 +0000

    update

--- components/Tour.js
+++ components/Tour.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Tour() {
@@ -8,3 +8,6 @@ export default function Tour() {
     </View>
   )
 }
+const styles = StyleSheet.create({
+    
+})
